00e2df9e94729987fb6a9c453d555ebe2965098d
unknown <mdnewman@usc.edu>
2019-10-26 21:10:52 -0700

    Add skeleton functions to DataManager, AuthenticationManager, User

--- app/src/main/java/com/cs310/usclassifieds/model/datamodel/User.java
+++ app/src/main/java/com/cs310/usclassifieds/model/datamodel/User.java
@@ -1,4 +1,9 @@
 package com.cs310.usclassifieds.model.datamodel;
 
 public class User {
+    private String passwordHash;
+
+    public String getPassword() {
+        return passwordHash;
+    }
 }
--- app/src/main/java/com/cs310/usclassifieds/model/manager/DataManager.java
+++ app/src/main/java/com/cs310/usclassifieds/model/manager/DataManager.java
@@ -1,4 +1,17 @@
 package com.cs310.usclassifieds.model.manager;
 
-public class DataManager {
+import com.cs310.usclassifieds.model.datamodel.User;
+
+class DataManager {
+    User getUser(int id) {
+        return new User();
+    }
+
+    User getUser(String username) {
+        return new User();
+    }
+
+    boolean userExists(String username) {
+        return getUser(username) != null;
+    }
 }
