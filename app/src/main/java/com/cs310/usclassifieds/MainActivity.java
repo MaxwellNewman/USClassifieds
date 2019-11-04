7b2d85f17ed905157fface346622740f995c6f1c
root <root@DESKTOP-RI2Q220.localdomain>
2019-11-04 11:20:31 -0800

    Can now access currentUser

--- app/src/main/java/com/cs310/usclassifieds/MainActivity.java
+++ app/src/main/java/com/cs310/usclassifieds/MainActivity.java
@@ -9,6 +9,7 @@ import android.view.View;
 import android.os.Bundle;
 import android.util.Log;
 
+import com.cs310.usclassifieds.model.manager.DataManager;
 import com.cs310.usclassifieds.ui.DataPassListener;
 import com.cs310.usclassifieds.model.datamodel.Item;
 import com.cs310.usclassifieds.model.datamodel.User;
@@ -19,14 +20,23 @@ import java.util.ArrayList;
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 import com.google.firebase.auth.FirebaseAuth;
 
+import com.cs310.usclassifieds.model.manager.UserManager;
+
 public class MainActivity extends AppCompatActivity implements DataPassListener {
 
     private List<Item> items = new ArrayList<Item>();
     private List<User> users = new ArrayList<User>();
     private Item viewedItem = new Item();
     private User viewedUser = new User();
+    private User currentUser = new User();
+    private UserManager userManager = new UserManager(new DataManager());
 
-    public String getCurrentUser(){
+
+    public User getCurrentUser() {
+        return currentUser;
+    }
+
+    public String getCurrentUserId(){
 
         return FirebaseAuth.getInstance().getCurrentUser().getUid();
     }
@@ -84,6 +94,7 @@ public class MainActivity extends AppCompatActivity implements DataPassListener 
         NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment);
         NavigationUI.setupActionBarWithNavController(this, navController, appBarConfiguration);
         NavigationUI.setupWithNavController(navView, navController);
+        this.currentUser = userManager.loadProfile(getCurrentUserId());
     }
 
     @Override
