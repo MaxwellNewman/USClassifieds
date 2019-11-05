520b4fddc173dea2ead73a2276348480609c18f2
camacker <camacker@usc.edu>
2019-11-05 14:26:37 -0800

    added notifications fragment

---
+++ app/src/main/java/com/cs310/usclassifieds/ui/notifications/NotificationsViewModel.java
@@ -0,0 +1,7 @@
+package com.cs310.usclassifieds.ui.notifications;
+
+import androidx.lifecycle.ViewModel;
+
+public class NotificationsViewModel extends ViewModel {
+    // TODO: Implement the ViewModel
+}
