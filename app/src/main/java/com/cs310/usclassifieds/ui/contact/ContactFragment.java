29f0c1ff624abc017c6708153397db8d5110ab4d
camacker <camacker@usc.edu>
2019-11-05 15:28:02 -0800

    linked contact and notifcations fragments

--- app/src/main/java/com/cs310/usclassifieds/ui/contact/ContactFragment.java
+++ app/src/main/java/com/cs310/usclassifieds/ui/contact/ContactFragment.java
@@ -1,5 +1,6 @@
 package com.cs310.usclassifieds.ui.contact;
 
+import androidx.lifecycle.ViewModel;
 import androidx.lifecycle.ViewModelProviders;
 
 import android.os.Bundle;
@@ -7,39 +8,138 @@ import android.os.Bundle;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.navigation.Navigation;
 
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 import com.cs310.usclassifieds.MainActivity;
 import com.cs310.usclassifieds.R;
 import com.cs310.usclassifieds.model.datamodel.User;
+import com.cs310.usclassifieds.model.manager.DataManager;
+import com.cs310.usclassifieds.model.manager.UserManager;
+import com.cs310.usclassifieds.ui.profile.ProfileFragment;
+import com.cs310.usclassifieds.ui.profile.ProfileViewModel;
+import com.squareup.picasso.Picasso;
 
-public class ContactFragment extends Fragment {
+import java.util.ArrayList;
+import java.util.List;
 
-    private ContactViewModel mViewModel;
+public class ContactFragment extends Fragment implements View.OnClickListener {
+
+//    private ContactViewModel mViewModel;
+//    private User user;
+//
+//    public static ContactFragment newInstance() {
+//        return new ContactFragment();
+//    }
+//
+//    @Override
+//    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container,
+//                             @Nullable Bundle savedInstanceState) {
+//        View view = inflater.inflate(R.layout.contact_fragment, container, false);
+//
+//        MainActivity activity = (MainActivity) getActivity();
+//
+//        // User that fragment is looking at
+////        user = activity.getViewedUser();
+////        TextView contactName = view.findViewById(R.id.contact_name);
+////        Log.v("Contact", user.username);
+////        contactName.setText(user.username);
+//
+//
+//        return view;
+//    }
+
+    private TextView usernameText;
+    private TextView emailText;
+    private Button viewFriendsButton;
+    private Button listingsButton;
+    private Button sendFriendRequestButton;
+    private ImageView imageView;
+
     private User user;
 
+    private ContactViewModel mViewModel;
+    private UserManager userManager = new UserManager(new DataManager());
     public static ContactFragment newInstance() {
         return new ContactFragment();
+    }
+
+
+    @Override
+    public void onClick(View view) {
+
+        MainActivity activity = (MainActivity) getActivity();
+        final User currentUser = activity.getCurrentUser();
+
+        List<User> friends = userManager.getFriendsOf(currentUser.userId);
+        if(friends == null) {
+            friends = new ArrayList<User>();
+        }
+
+        activity.passUsers(friends);
+
+        Navigation.findNavController(view).navigate(R.id.action_navigation_contact_to_navigation_friend_results);
     }
 
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container,
                              @Nullable Bundle savedInstanceState) {
-        View view = inflater.inflate(R.layout.contact_fragment, container, false);
 
         MainActivity activity = (MainActivity) getActivity();
+        final User currentUser = activity.getCurrentUser();
+
+        mViewModel = ViewModelProviders.of(this).get(ContactViewModel.class);
+        View view = inflater.inflate(R.layout.contact_fragment, container, false);
 
         // User that fragment is looking at
         user = activity.getViewedUser();
-        TextView contactName = view.findViewById(R.id.contact_name);
-        Log.v("Contact", user.username);
-        contactName.setText(user.username);
+//        TextView contactName = view.findViewById(R.id.contact_name);
+//        Log.v("Contact", user.username);
+//        contactName.setText(user.username);
 
+        usernameText = view.findViewById(R.id.contact_username_profile);
+        emailText = view.findViewById(R.id.contact_email_profile);
+//        findFriendsButton = view.findViewById(R.id.find_friends_button);
+        viewFriendsButton = view.findViewById(R.id.contact_find_friends_button);
+        listingsButton = view.findViewById(R.id.contact_listings_button);
+        sendFriendRequestButton = view.findViewById(R.id.add_friend_button);
+
+        usernameText.append(user.username);
+        emailText.append(user.contactInfo.email);
+
+        viewFriendsButton.setOnClickListener(this);
+
+        listingsButton.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View view) {
+                // Need to create new fragment for this, view_listing currently views a listing from search
+                //Navigation.findNavController(view).navigate(R.id.navigation_view_listing);
+                //TODO load the users listings
+                Navigation.findNavController(view).navigate(R.id.action_navigation_contact_to_navigation_results);
+            }
+        });
+
+        sendFriendRequestButton.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View view) {
+            //TODO this is where you should send the friend request
+            }
+        });
+
+        imageView = view.findViewById(R.id.contact_profile_image_view);
+
+        final String url = currentUser.imageUrl == null ?
+                MainActivity.DEFAULT_URL :
+                currentUser.imageUrl;
+
+        Picasso.with(getContext()).load(url).into(imageView);
 
         return view;
     }
