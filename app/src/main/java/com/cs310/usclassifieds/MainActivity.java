1804a2a250eaf9924834b0aa44295d57ccb385f0
root <root@DESKTOP-RI2Q220.localdomain>
2019-11-04 16:04:20 -0800

    Added basic map search functionality

--- app/src/main/java/com/cs310/usclassifieds/MainActivity.java
+++ app/src/main/java/com/cs310/usclassifieds/MainActivity.java
@@ -9,7 +9,6 @@ import android.view.View;
 import android.os.Bundle;
 import android.util.Log;
 
-import com.cs310.usclassifieds.model.manager.DataManager;
 import com.cs310.usclassifieds.ui.DataPassListener;
 import com.cs310.usclassifieds.model.datamodel.Item;
 import com.cs310.usclassifieds.model.datamodel.User;
@@ -20,21 +19,12 @@ import java.util.ArrayList;
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 import com.google.firebase.auth.FirebaseAuth;
 
-import com.cs310.usclassifieds.model.manager.UserManager;
-
 public class MainActivity extends AppCompatActivity implements DataPassListener {
 
     private List<Item> items = new ArrayList<Item>();
     private List<User> users = new ArrayList<User>();
     private Item viewedItem = new Item();
     private User viewedUser = new User();
-    private User currentUser = new User();
-    private UserManager userManager = new UserManager(new DataManager());
-
-
-    public User getCurrentUser() {
-        return currentUser;
-    }
 
     public String getCurrentUserId(){
 
@@ -94,7 +84,6 @@ public class MainActivity extends AppCompatActivity implements DataPassListener 
         NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment);
         NavigationUI.setupActionBarWithNavController(this, navController, appBarConfiguration);
         NavigationUI.setupWithNavController(navView, navController);
-        this.currentUser = userManager.loadProfile(getCurrentUserId());
     }
 
     @Override
--- app/src/main/java/com/cs310/usclassifieds/ui/map/MapFragment.java
+++ app/src/main/java/com/cs310/usclassifieds/ui/map/MapFragment.java
@@ -7,38 +7,118 @@ import android.os.Bundle;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.navigation.Navigation;
 
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.EditText;
 
+import com.cs310.usclassifieds.MainActivity;
 import com.cs310.usclassifieds.R;
+import com.cs310.usclassifieds.model.datamodel.Item;
+import com.cs310.usclassifieds.model.manager.DataManager;
+import com.cs310.usclassifieds.model.manager.SearchManager;
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.MapView;
+import com.google.android.gms.maps.MapsInitializer;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.model.CameraPosition;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.Marker;
+import com.google.android.gms.maps.model.MarkerOptions;
 
-public class MapFragment extends Fragment {
+import java.util.List;
+
+public class MapFragment extends Fragment implements OnMapReadyCallback, GoogleMap.OnMarkerClickListener {
 
     private MapViewModel mViewModel;
+    private GoogleMap googleMap;
+    private MapView mMapView;
+    private List<Item> items;
+    private SearchManager searchManager = new SearchManager(new DataManager());
+    private EditText searchText;
+    private View view;
 
     public static MapFragment newInstance() {
         return new MapFragment();
+    }
+
+    // When a marker is clicked
+    @Override
+    public boolean onMarkerClick(final Marker marker) {
+
+        // Navigate to the listing clicked
+        for (Item item : items)
+        {
+            // Not necessarily the correct item, but it matches the marker in all fields
+            // Can fix this in case 2 people sell the exact same item with the same description
+            // in the exact same place (seems unlikely)
+            if (marker.getTitle().equals(item.title) && marker.getSnippet().equals(
+                    item.description) && marker.getPosition().latitude ==
+                            item.location.latitude && marker.getPosition().longitude ==
+                            item.location.longitude) {
+                MainActivity activity = (MainActivity) getActivity();
+                activity.setViewedItem(item);
+                Navigation.findNavController(this.view).navigate(R.id.navigation_view_listing);
+                return true;
+            }
+        }
+
+        return false;
+    }
+
+    @Override
+    public void onMapReady(GoogleMap mMap) {
+        this.googleMap = mMap;
+        String searchText = this.searchText.getText().toString();
+        Log.v("Map Search Text", searchText);
+        googleMap.setOnMarkerClickListener(this);
+
+        // Place item markers on the map
+        items = searchManager.searchItemsByTitle(searchText);
+        for (Item item : items){
+            LatLng itemLatLng = new LatLng(item.location.latitude, item.location.longitude);
+            googleMap.addMarker(new MarkerOptions().position(itemLatLng).title(
+                    item.title).snippet(item.description));
+    }
+
+        // Zoom automatically to usc
+        LatLng usc = new LatLng(34.0211917, -118.2863514);
+        CameraPosition cameraPosition = new CameraPosition.Builder().target(usc).zoom((float)15).build();
+        googleMap.animateCamera(CameraUpdateFactory.newCameraPosition(cameraPosition));
+
     }
 
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container,
                              @Nullable Bundle savedInstanceState) {
         mViewModel = ViewModelProviders.of(this).get(MapViewModel.class);
-        View view = inflater.inflate(R.layout.map_fragment, container, false);
+        this.view = inflater.inflate(R.layout.map_fragment, container, false);
+        this.searchText = (EditText) view.findViewById(R.id.map_search_bar);
 
-//        Button loadListingButton = (Button) view.findViewById(R.id.placeholder_load_listing_button);
-//        loadListingButton.setOnClickListener(new View.OnClickListener(){
-//            @Override
-//            public void onClick(View view) {
-//                //TODO call the database and pass data
-//                //TODO (btw you need to do it for all of them, I'm not about to make a million todos)
-//                Navigation.findNavController(view).navigate(R.id.navigation_view_listing);
-//            }
-//        });
+        // Load items from searchText (should default to load all items)
 
-        return view;
+        Log.v("searching for:", searchText.getText().toString());
+
+        //this.items = searchManager.searchItemsByTitle(searchText.getText().toString());
+
+        this.mMapView = (MapView) view.findViewById(R.id.map_of_listings);
+        this.mMapView.onCreate(savedInstanceState);
+
+        this.mMapView.onResume();
+
+        try {
+            MapsInitializer.initialize(getActivity().getApplicationContext());
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        mMapView.getMapAsync(this);
+
+        return this.view;
     }
 
     @Override
