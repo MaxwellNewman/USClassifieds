0885e34cc903174608437c711a51f7956169d516
root <root@DESKTOP-RI2Q220.localdomain>
2019-11-05 16:23:43 -0800

    Removed back button that did not work.

--- app/src/main/java/com/cs310/usclassifieds/MainActivity.java
+++ app/src/main/java/com/cs310/usclassifieds/MainActivity.java
@@ -148,6 +148,7 @@ public class MainActivity extends AppCompatActivity implements DataPassListener 
 
         setContentView(R.layout.activity_main);
         BottomNavigationView navView = findViewById(R.id.nav_view);
+
         // Passing each menu ID as a set of Ids because each
         // menu should be considered as top level destinations.
         AppBarConfiguration appBarConfiguration = new AppBarConfiguration.Builder(
@@ -156,7 +157,7 @@ public class MainActivity extends AppCompatActivity implements DataPassListener 
 
         NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment);
 
-        NavigationUI.setupActionBarWithNavController(this, navController, appBarConfiguration);
+        //NavigationUI.setupActionBarWithNavController(this, navController, appBarConfiguration);
         NavigationUI.setupWithNavController(navView, navController);
 
     }
