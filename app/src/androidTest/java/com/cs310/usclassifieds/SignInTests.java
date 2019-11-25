5c93327d93a0d64d52aa3112c3bbf9d3d761be1b
camacker <camacker@usc.edu>
2019-11-24 20:46:45 -0800

    put all the sign in tests in a single file

--- app/src/androidTest/java/com/cs310/usclassifieds/SignInTests.java
+++ app/src/androidTest/java/com/cs310/usclassifieds/SignInTests.java
@@ -1,9 +1,6 @@
 package com.cs310.usclassifieds;
 
 
-import androidx.test.espresso.Espresso;
-import androidx.test.espresso.IdlingResource;
-import androidx.test.espresso.ViewInteraction;
 import androidx.test.filters.LargeTest;
 import androidx.test.rule.ActivityTestRule;
 import androidx.test.runner.AndroidJUnit4;
@@ -24,15 +21,41 @@ import static androidx.test.espresso.matcher.ViewMatchers.withText;
 
 @LargeTest
 @RunWith(AndroidJUnit4.class)
-public class CreateExistingAccountTest {
+public class SignInTests {
 
     private static final String USERNAME = "eTest";
     private static final String PASSWORD = "password";
     private static final String ACCOUNT_NAME = "Espresso Test Account";
     private static final String EMAIL = "espressotest@usc.edu";
 
+    private static final String BAD_USERNAME = "Not a real username";
+    private static final String BAD_PASSWORD = "bad password";
+
     @Rule
     public ActivityTestRule<SignInActivity> mActivityTestRule = new ActivityTestRule<>(SignInActivity.class);
+
+    @Test
+    public void SignInTest() throws InterruptedException {
+
+        //type in login info and hit submit
+        onView(withId(R.id.username)).perform(replaceText(USERNAME));
+        onView(withId(R.id.password)).perform(replaceText(PASSWORD));
+        onView(withId(R.id.login)).perform(click());
+
+        //current implementation just sleeps for 15s while we do firebase calls
+        //we could use idle resources but idk how (or if its worth figuring out how)
+        //15 seconds is the an arbitrary time, I figure the google auth should be done by now and the next page will have loaded
+        Thread.sleep(15000);
+
+        //change page to profile
+        onView(withId(R.id.navigation_profile)).perform(click());
+
+        //confirm that the profile page has the correct user information
+        onView(withId(R.id.name_text)).check(matches((withText(ACCOUNT_NAME))));
+        onView(withId(R.id.email_profile)).check(matches((withText("Email: " + EMAIL))));
+        onView(withId(R.id.username_profile)).check(matches((withText("Username: " + USERNAME))));
+
+    }
 
     @Test
     public void CreateExistingAccountTest() throws InterruptedException {
@@ -75,5 +98,22 @@ public class CreateExistingAccountTest {
         onView(withId(R.id.email_profile)).check(matches((withText("Email: " + EMAIL))));
         onView(withId(R.id.username_profile)).check(matches((withText("Username: " + USERNAME))));
 
+    }
+
+    @Test
+    public void InvalidSignInTest() throws InterruptedException {
+
+        //type in login info and hit submit
+        onView(withId(R.id.username)).perform(replaceText(BAD_USERNAME));
+        onView(withId(R.id.password)).perform(replaceText(BAD_PASSWORD));
+        onView(withId(R.id.login)).perform(click());
+
+        //current implementation just sleeps for 15s while we do firebase calls
+        //we could use idle resources but idk how (or if its worth figuring out how)
+        //15 seconds is the an arbitrary time, I figure the google auth should be done by now and the next page will have loaded
+        Thread.sleep(15000);
+
+        //check that the login button is displayed (aka we haven't left the login page)
+        onView(withId(R.id.login)).check(matches(isDisplayed()));
     }
 }
