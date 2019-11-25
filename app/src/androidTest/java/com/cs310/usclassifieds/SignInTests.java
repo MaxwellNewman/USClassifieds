79ee2b8149a29c49a8702606b2bde5c9b9e689ef
camacker <camacker@usc.edu>
2019-11-24 21:27:10 -0800

    added more tests to cover fringe cases

--- app/src/androidTest/java/com/cs310/usclassifieds/SignInTests.java
+++ app/src/androidTest/java/com/cs310/usclassifieds/SignInTests.java
@@ -28,8 +28,10 @@ public class SignInTests {
     private static final String ACCOUNT_NAME = "Espresso Test Account";
     private static final String EMAIL = "espressotest@usc.edu";
 
-    private static final String BAD_USERNAME = "Not a real username";
-    private static final String BAD_PASSWORD = "bad password";
+    private static final String BAD_USERNAME = "BadUsername";
+    private static final String BAD_PASSWORD = "BadPassword";
+    private static final String BAD_ACCOUNT_NAME = "Bad Account Name";
+    private static final String BAD_EMAIL = "bademail@usc.edu";
 
     @Rule
     public ActivityTestRule<SignInActivity> mActivityTestRule = new ActivityTestRule<>(SignInActivity.class);
@@ -57,8 +59,10 @@ public class SignInTests {
 
     }
 
+    //these tests are repetitive, but it is better to split them into multiple tests
+    //so that we would be identify the problem(s) based on the test results
     @Test
-    public void CreateExistingAccountTest() throws InterruptedException {
+    public void CreateExistingAccountTest1() throws InterruptedException {
         //click on create account button
         onView(withId(R.id.textView8)).perform(click());
 
@@ -101,10 +105,156 @@ public class SignInTests {
     }
 
     @Test
-    public void InvalidSignInTest() throws InterruptedException {
+    public void CreateExistingAccountTest2() throws InterruptedException {
+        //click on create account button
+        onView(withId(R.id.textView8)).perform(click());
+
+        //type in account info and hit submit
+        onView(withId(R.id.createUsername)).perform(replaceText(USERNAME));
+        onView(withId(R.id.createPassword)).perform(replaceText(PASSWORD));
+        onView(withId(R.id.createEmail)).perform(replaceText(BAD_EMAIL));
+        onView(withId(R.id.createName)).perform(replaceText(ACCOUNT_NAME));
+        onView(withId(R.id.createButton)).perform(click());
+
+        //current implementation just sleeps for 15s while we do firebase calls
+        //we could use idle resources but idk how (or if its worth figuring out how)
+        //15 seconds is the an arbitrary time, I figure the google auth should be done by now and the next page will have loaded
+        Thread.sleep(10000);
+
+        //check that the create account button is displayed (aka we did not make an account since it already existed)
+        onView(withId(R.id.createButton)).check(matches(isDisplayed()));
+
+        //now we can go back to the login page to confirm that the user does indeed exist
+        onView(withId(R.id.signInClickableText)).perform(click());
+
+        //type in login info and hit submit
+        onView(withId(R.id.username)).perform(replaceText(USERNAME));
+        onView(withId(R.id.password)).perform(replaceText(PASSWORD));
+        onView(withId(R.id.login)).perform(click());
+
+        //current implementation just sleeps for 15s while we do firebase calls
+        //we could use idle resources but idk how (or if its worth figuring out how)
+        //15 seconds is the an arbitrary time, I figure the google auth should be done by now and the next page will have loaded
+        Thread.sleep(15000);
+
+        //change page to profile
+        onView(withId(R.id.navigation_profile)).perform(click());
+
+        //confirm that the profile page has the correct user information
+        onView(withId(R.id.name_text)).check(matches((withText(ACCOUNT_NAME))));
+        onView(withId(R.id.email_profile)).check(matches((withText("Email: " + EMAIL))));
+        onView(withId(R.id.username_profile)).check(matches((withText("Username: " + USERNAME))));
+
+    }
+
+    @Test
+    public void CreateExistingAccountTest3() throws InterruptedException {
+        //click on create account button
+        onView(withId(R.id.textView8)).perform(click());
+
+        //type in account info and hit submit
+        onView(withId(R.id.createUsername)).perform(replaceText(BAD_USERNAME));
+        onView(withId(R.id.createPassword)).perform(replaceText(PASSWORD));
+        onView(withId(R.id.createEmail)).perform(replaceText(EMAIL));
+        onView(withId(R.id.createName)).perform(replaceText(ACCOUNT_NAME));
+        onView(withId(R.id.createButton)).perform(click());
+
+        //current implementation just sleeps for 15s while we do firebase calls
+        //we could use idle resources but idk how (or if its worth figuring out how)
+        //15 seconds is the an arbitrary time, I figure the google auth should be done by now and the next page will have loaded
+        Thread.sleep(10000);
+
+        //check that the create account button is displayed (aka we did not make an account since it already existed)
+        onView(withId(R.id.createButton)).check(matches(isDisplayed()));
+
+        //now we can go back to the login page to confirm that the user does indeed exist
+        onView(withId(R.id.signInClickableText)).perform(click());
+
+        //type in login info and hit submit
+        onView(withId(R.id.username)).perform(replaceText(USERNAME));
+        onView(withId(R.id.password)).perform(replaceText(PASSWORD));
+        onView(withId(R.id.login)).perform(click());
+
+        //current implementation just sleeps for 15s while we do firebase calls
+        //we could use idle resources but idk how (or if its worth figuring out how)
+        //15 seconds is the an arbitrary time, I figure the google auth should be done by now and the next page will have loaded
+        Thread.sleep(15000);
+
+        //change page to profile
+        onView(withId(R.id.navigation_profile)).perform(click());
+
+        //confirm that the profile page has the correct user information
+        onView(withId(R.id.name_text)).check(matches((withText(ACCOUNT_NAME))));
+        onView(withId(R.id.email_profile)).check(matches((withText("Email: " + EMAIL))));
+        onView(withId(R.id.username_profile)).check(matches((withText("Username: " + USERNAME))));
+
+    }
+
+    @Test
+    public void CreateExistingAccountTest4() throws InterruptedException {
+        //click on create account button
+        onView(withId(R.id.textView8)).perform(click());
+
+        //type in account info and hit submit
+        onView(withId(R.id.createUsername)).perform(replaceText(USERNAME));
+        onView(withId(R.id.createPassword)).perform(replaceText(PASSWORD));
+        onView(withId(R.id.createEmail)).perform(replaceText(EMAIL));
+        onView(withId(R.id.createName)).perform(replaceText(BAD_ACCOUNT_NAME));
+        onView(withId(R.id.createButton)).perform(click());
+
+        //current implementation just sleeps for 15s while we do firebase calls
+        //we could use idle resources but idk how (or if its worth figuring out how)
+        //15 seconds is the an arbitrary time, I figure the google auth should be done by now and the next page will have loaded
+        Thread.sleep(10000);
+
+        //check that the create account button is displayed (aka we did not make an account since it already existed)
+        onView(withId(R.id.createButton)).check(matches(isDisplayed()));
+
+        //now we can go back to the login page to confirm that the user does indeed exist
+        onView(withId(R.id.signInClickableText)).perform(click());
+
+        //type in login info and hit submit
+        onView(withId(R.id.username)).perform(replaceText(USERNAME));
+        onView(withId(R.id.password)).perform(replaceText(PASSWORD));
+        onView(withId(R.id.login)).perform(click());
+
+        //current implementation just sleeps for 15s while we do firebase calls
+        //we could use idle resources but idk how (or if its worth figuring out how)
+        //15 seconds is the an arbitrary time, I figure the google auth should be done by now and the next page will have loaded
+        Thread.sleep(15000);
+
+        //change page to profile
+        onView(withId(R.id.navigation_profile)).perform(click());
+
+        //confirm that the profile page has the correct user information
+        onView(withId(R.id.name_text)).check(matches((withText(ACCOUNT_NAME))));
+        onView(withId(R.id.email_profile)).check(matches((withText("Email: " + EMAIL))));
+        onView(withId(R.id.username_profile)).check(matches((withText("Username: " + USERNAME))));
+
+    }
+
+    @Test
+    public void InvalidSignInTest1() throws InterruptedException {
 
         //type in login info and hit submit
         onView(withId(R.id.username)).perform(replaceText(BAD_USERNAME));
+        onView(withId(R.id.password)).perform(replaceText(BAD_PASSWORD));
+        onView(withId(R.id.login)).perform(click());
+
+        //current implementation just sleeps for 15s while we do firebase calls
+        //we could use idle resources but idk how (or if its worth figuring out how)
+        //15 seconds is the an arbitrary time, I figure the google auth should be done by now and the next page will have loaded
+        Thread.sleep(15000);
+
+        //check that the login button is displayed (aka we haven't left the login page)
+        onView(withId(R.id.login)).check(matches(isDisplayed()));
+    }
+
+    @Test
+    public void InvalidSignInTest2() throws InterruptedException {
+
+        //type in login info and hit submit
+        onView(withId(R.id.username)).perform(replaceText(USERNAME));
         onView(withId(R.id.password)).perform(replaceText(BAD_PASSWORD));
         onView(withId(R.id.login)).perform(click());
 
